ca9fb0da6fcc94410c165bc4792d27b1edc9b1d3
Pat Jameson <patrickejameson@gmail.com>
2013-06-20 13:16:30 -0700

    Initial commit. Can handle handshakes between two clients

---
+++ app.js
@@ -0,0 +1,94 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , app = express()
+  , server = require('http').createServer(app)
+  , io = require('socket.io').listen(server)
+  , routes = require('./routes')
+  , path = require('path')
+  , mu2Express = require('mu2express');
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.engine('mustache', mu2Express.engine);
+app.set('view engine', 'mustache');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+app.get('/', routes.webrtc);
+app.get('/:id', routes.webrtc);
+
+var sessions = {};
+var sockets = {};
+
+io.sockets.on('connection', function (socket) {
+	socket.on('desc', function(data) {
+		//up to sockets.length-2 because sockets[sockets.length-1] is the current socket.
+		for (var i = 0;i < sockets[data.id].length-1;i++) {
+			sockets[data.id][i].emit('add_desc', {'id': data.id, 'client_id': data.client_id, 'desc': data.desc});
+
+			for (var j = 0;j < sessions[data.id][i]['cand'].length;j++) {
+				sockets[data.id][sockets[data.id].length-1].emit('add_cand', {'cand': sessions[data.id][i]['cand'][j]});
+			}
+		}
+		
+		sessions[data.id][data.client_id]['desc'] = data.desc;
+	});
+
+	socket.on('cand', function(data) {
+		//up to sockets.length-2 because sockets[sockets.length-1] is the current socket.
+		for (var i = 0;i < sockets[data.id].length-1;i++) {
+			sockets[data.id][i].emit('add_cand', {'cand': data.cand});
+		}
+		sessions[data.id][data.client_id]['cand'].push(data.cand);
+	});
+
+	socket.on('create_room', function(data) {
+		
+	});
+
+	socket.on('join', function(data) {
+		console.log('JOIN');
+		if (data.id !== '' && sockets[data.id] !== undefined) {
+			client_id = sockets[data.id].length;
+			id = data.id;
+
+			console.log(sessions);
+			for (var i = 0;i < sessions[data.id].length;i++) {
+				socket.emit('add_desc', {'id': data.id, 'client_id': client_id, 'desc': sessions[data.id][i].desc});
+			}
+		} else {
+			var id;
+			do {
+				id = Math.floor(Math.random()*1000000);
+			} while (id in sessions);
+
+			client_id = 0;
+		}
+
+		if (sessions[id] === undefined) {
+			sessions[id] = [];
+			sockets[id] = [];
+		}
+		sessions[id].push({'client_id': client_id, 'desc': '', 'cand': []});
+		sockets[id].push(socket);
+		socket.emit('joined', {'client_id': client_id, 'id': id});
+	});
+});
+
+server.listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
---
+++ public/javascripts/webrtc/webrtc.js
@@ -0,0 +1,191 @@
+YUI.add('webrtc', function (Y, NAME) {
+
+var socket = io.connect('/');
+var sessions, pc1, id, dc1;
+
+var DEFAULT_STUN_SERVER = {'url': 'stun:stun.l.google.com:19302'};
+
+var _localStreamElement = null,
+	_remoteStreamElement = null,
+	_roomId = '',
+	clientId = -1,
+	_isStreaming = false,
+	_localStream = null,
+	_remoteStream = null,
+	_isDataChannelOpen = false,
+	_disconnectedMessages = [],
+	_owner = true;
+
+function WebRTC() {}
+
+WebRTC.prototype = {
+	startStream: function (type, callback) {
+		var configuration = {'iceServers': [DEFAULT_STUN_SERVER]};
+		var connection = {'optional': [{'RtpDataChannels': true }]};
+		pc1 = new RTCPeerConnection(configuration, connection);
+
+		thisWebRTC = this;
+
+		if (type == 'both' || type == 'media' || typeof type === undefined) {
+			thisWebRTC._startLocalStream(function() {
+				pc1.addStream(_localStream);
+
+				pc1.onaddstream = function (event) {
+					_remoteStream = event.stream;
+					attachMediaStream(_remoteStreamElement, _remoteStream);
+					_isStreaming = true;
+				};
+
+				if (type == 'both' || typeof type === undefined) {
+					dc1 = pc1.createDataChannel('test', {'reliable': false});
+
+					thisWebRTC._startDataChannel();
+				}
+
+				thisWebRTC._makeConnection(callback);
+			});
+		} else if (type == 'data') {
+			dc1 = pc1.createDataChannel('test', {'reliable': false});
+			
+			thisWebRTC._startDataChannel();
+
+			thisWebRTC._makeConnection(callback);
+		}
+	},
+
+	setDataChannelCallback: function(callback) {
+		dc1.onmessage = callback;
+	},
+
+	send: function(message) {
+		if (_isDataChannelOpen)
+			dc1.send(message);
+		else
+			_disconnectedMessages.push(message);
+	},
+
+	setLocal: function(element) {
+		_localStreamElement = document.getElementById(element);
+	},
+
+	setRemote: function(element) {
+		_remoteStreamElement = document.getElementById(element);
+	},
+
+	pause: function() {
+		pc1.removeStream(_localStream);
+		this._updateDescription();
+		_isStreaming = false;
+	},
+
+	isStreaming: function() {
+		return paused;
+	},
+
+	resume: function() {
+		pc1.addStream(_localStream);
+		this._updateDescription();
+		_isStreaming = true;
+	},
+
+	mute: function() {
+
+	},
+
+	getId: function() {
+		return _roomId;
+	},
+
+	setId: function(roomId) {
+		_owner = false;
+		_roomId = roomId;
+	},
+
+	setIceServers: function(servers) {
+		if (typeof servers === 'array') {
+			_ice_servers = servers;
+		} else {
+			_ice_servers = [servers];
+		}
+	},
+
+	isStreaming: function() {
+		return _streaming;
+	},
+
+	_startLocalStream: function (callback) {
+		getUserMedia({audio: true, video: true}, function (stream) {
+			_localStream = stream;
+
+			attachMediaStream(_localStreamElement, _localStream);
+
+			callback();
+		});
+	},
+
+	_startDataChannel: function () {
+		dc1.onmessage = null;
+
+		dc1.onopen = function () {
+			_isDataChannelOpen = true;
+			for (var i = 0;i < _disconnectedMessages.length;i++) {
+				dc1.send(_disconnectedMessages[i]);
+			}
+		};
+	},
+
+	_makeConnection: function (callback) {
+		var thisWebRTC = this;
+		pc1.onicecandidate = function (event) {
+			if (event.candidate) {
+				socket.emit('cand', {'client_id': client_id, 'cand': event.candidate, 'id': id});
+			}
+		};
+
+		socket.emit('join', {'id': _roomId});
+
+		socket.on('joined', function (data) {
+			client_id = data.client_id;
+			id = data.id;
+
+			if (_roomId === '') {
+				thisWebRTC._updateDescription();
+			}
+
+			callback(id);
+		});
+
+		socket.on('add_desc', function (data) {
+			client_id = data.client_id;
+			id = data.id;
+
+			pc1.setRemoteDescription(new RTCSessionDescription(data.desc));
+
+			_streaming = true;
+
+			if (_roomId !== '') {
+				pc1.createAnswer(function (desc) {
+					pc1.setLocalDescription(desc);
+					socket.emit('desc', {'client_id': client_id, 'desc': desc, 'id': id});
+				});
+			}
+		});
+
+		socket.on('add_cand', function (data) {
+			pc1.addIceCandidate(new RTCIceCandidate(data.cand));
+		});
+	},
+
+	_updateDescription: function() {
+		pc1.createOffer(function(desc) {
+			pc1.setLocalDescription(desc);
+			socket.emit('desc', {'client_id': client_id, 'desc': desc, 'id': id});
+		}, null);
+	}
+};
+
+Y.WebRTC = WebRTC;
+
+
+
+}, '@VERSION@', {"requires": ["node", "socket_io"]});
